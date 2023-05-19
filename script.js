2d185824145a0b210f32099312fe7604043c781a
JordanLindley <jlindley94@gmail.com>
2023-05-18 22:21:24 -0400

    Add Book button displays form. Capture form elements in function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // DOM elements:
-// const titleInput = document.querySelector('.title-input').value;
-// const authorInput = document.querySelector('.author-input').value;
+const titleInput = document.querySelector('.title-input').value;
+const authorInput = document.querySelector('.author-input').value;
 const libraryDisplay = document.querySelector('.library');
 const addBookButton = document.querySelector('.add-book');
 const createBookBox = document.querySelector('.create-book');
+const submit = document.querySelector('.create-btn');
 
 // book constructor
 function Book(id, title, author, pages, haveRead) {
@@ -18,18 +19,41 @@ function Book(id, title, author, pages, haveRead) {
 let library = [];
 
 // push created book to library
-const addBook = newBook => library.push(newBook);
+// const addBook = newBook => library.push(newBook);
+const addBook = (e) => {
+  e.preventDefault();
+
+  // Book form data
+  let title = createBookBox.title.value;
+  let author = createBookBox.author.value;
+  let pages = createBookBox.pages.value;
+  let read;
+  formDisplay.read.checked ? read = "Read" : read = "Not Read";
+
+  let newBook = new Book(id, title, author, pages, haveRead);
+  library.push(newBook);
+  formDisplay.reset();
+
+
+}
+
+// delete a book based on its index
+function removeBook(book) {
+  const index = library.indexOf(book);
+  library.splice(index, 1);
+}
 
 // placeholder books
-const theHobbit = new Book(0, 'The Hobbit', 'J.R.R. Tolkien', 304, false);
-const handmaidsTale = new Book(1, 'The Handmaid\'s Tale', 'Margaret Atwood', 314, true);
-addBook(theHobbit);
-addBook(handmaidsTale);
+// const theHobbit = new Book(0, 'The Hobbit', 'J.R.R. Tolkien', 304, false);
+// const handmaidsTale = new Book(1, 'The Handmaid\'s Tale', 'Margaret Atwood', 314, true);
+// addBook(theHobbit);
+// addBook(handmaidsTale);
 
 // populate divs
+// this  would be much easer in React.js - we'll get there...
 function displayBooks() {
   for (let i = 0; i < library.length; i++) {
-    let deleteBook = document.createElement('button').className('delete-book');
+    let deleteBook = document.createElement('button');
     let book = document.createElement('div');
     let title = document.createElement('h1');
     let author = document.createElement('h2');
@@ -39,7 +63,7 @@ function displayBooks() {
     let readRound = document.createElement('div');
     
     // add class to divs
-    book.className = 'book';
+    book.className ='book';
     title.className = 'book-title';
     author.className = 'book-author';
     pages.className = 'book-pages';
@@ -47,6 +71,7 @@ function displayBooks() {
     readSwitch.className = 'switch';
     readSwitch.setAttribute('type', 'checkbox');
     readRound.className = 'have-read-slider round';
+    deleteBook.className = 'delete';
     
     // display text of book cards
     title.textContent = `${library[i].title}`;
@@ -73,22 +98,17 @@ function displayBooks() {
     })
 
       // tried to implement a delete button but nothing fucking works now.
-    // function popBook(book) {
-    //   const index = library.indexOf(book);
-    //   library.splice(index, 1);
-    // }
-
-    // deleteBook.addEventListener('click', function() {
-    //   popBook(library[i]);
-    // })
+    deleteBook.addEventListener('click', () => {
+      popBook(library[i]);
+    })
   }
 }
  
 displayBooks();
 
 function showBox() {
-  if (createBookBox.style.display === 'none') {
-    createBookBox.style.display = 'block';
+  if (createBookBox.style.display == 'none') {
+    createBookBox.style.display = 'grid';
   } else createBookBox.style.display = 'none';
 }
 
